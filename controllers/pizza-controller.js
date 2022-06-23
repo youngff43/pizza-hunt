cde593b02dfdace0c82729f78ffa6aae3824dee9
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-06-22 23:52:21 -0400

    added comments routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+      .select('-__v')
+      .sort({_id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -11,22 +17,26 @@ const pizzaController = {
       });
   },
 
-  // get one pizza by id
-  getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => {
-        // If no pizza is found, send 404
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
+// get one pizza by id
+getPizzaById({ params }, res) {
+  Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+},
 
   // createPizza
   createPizza({ body }, res) {
--- server.js
+++ server.js
@@ -16,7 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
   useUnifiedTopology: true
 });
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
-
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
